3bdbd0f42a80cfa60601e45e07762fb2d6918dc8
Martin Karlsson <xenovoyance@gmail.com>
2020-12-01 16:22:11 +0100

    Day 1 completed.

--- aoc2020-python/day1/day1.py
+++ aoc2020-python/day1/day1.py
@@ -1,5 +1,7 @@
-# Global
-run_env = "test" ## test or prod
+import itertools
+
+
+run_env = "prod" ## test or prod
 
 if run_env == "test":
     input = "/Users/martin.karlsson/Dropbox/Code/Advent of code/aoc2020-python/day1/test_input.txt"
@@ -11,16 +13,26 @@ list_counter = 0
 
 answer_numbers = []
 
-# Main loop goes here
-with open(input) as blockstream:
-    for stream in blockstream:
-        stream.rstrip(' ,\n\r')
-        digit_list.append(int(stream))
+def init():
+    # Main loop goes here
+    with open(input) as blockstream:
+        for stream in blockstream:
+            stream.rstrip(' ,\n\r')
+            digit_list.append(int(stream))
 
-for digit in digit_list:
-    sum = 2020 - digit
-    if sum in digit_list:
-        answer_numbers.append(sum)
+def part1():
+    for digit in digit_list:
+        sum = 2020 - digit
+        if sum in digit_list:
+            answer_numbers.append(sum)
 
-#print(answer_numbers)
-print(answer_numbers[0]*answer_numbers[1])
+    print("Part 1: " + str(answer_numbers[0]*answer_numbers[1]))
+
+def part2():
+    for a, b, c in itertools.combinations(digit_list, 3):
+        if a+b+c == 2020:
+          print("Part 2: " + str(a*b*c))
+
+init()
+part1()
+part2()
