4cb941fb73010de50421e68e57380cccbaf5edb3
Martin Karlsson <xenovoyance@gmail.com>
2020-12-01 07:24:12 +0100

    asdf

--- aoc2020-python/day1/day1.py
+++ aoc2020-python/day1/day1.py
@@ -4,9 +4,7 @@ run_env = "test" ## test or prod
 if run_env == "test":
     input = "test_input.txt"
 else:
-    input = "input_daynine.txt"
-
-print()
+    input = "day1.txt"
 
 # Main loop goes here
 with open(input) as blockstream:
