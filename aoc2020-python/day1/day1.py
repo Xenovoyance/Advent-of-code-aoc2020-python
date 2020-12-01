37d40c1377c64554ef171fea6138ee673c1621ef
Martin Karlsson <xenovoyance@gmail.com>
2020-12-01 10:58:03 +0100

    Day 1, part 1 complete

--- aoc2020-python/day1/day1.py
+++ aoc2020-python/day1/day1.py
@@ -2,12 +2,25 @@
 run_env = "test" ## test or prod
 
 if run_env == "test":
-    input = "test_input.txt"
+    input = "/Users/martin.karlsson/Dropbox/Code/Advent of code/aoc2020-python/day1/test_input.txt"
 else:
-    input = "day1.txt"
+    input = "/Users/martin.karlsson/Dropbox/Code/Advent of code/aoc2020-python/day1/day1.txt"
+
+digit_list = []
+list_counter = 0
+
+answer_numbers = []
 
 # Main loop goes here
 with open(input) as blockstream:
     for stream in blockstream:
         stream.rstrip(' ,\n\r')
-        print(stream)
+        digit_list.append(int(stream))
+
+for digit in digit_list:
+    sum = 2020 - digit
+    if sum in digit_list:
+        answer_numbers.append(sum)
+
+#print(answer_numbers)
+print(answer_numbers[0]*answer_numbers[1])
